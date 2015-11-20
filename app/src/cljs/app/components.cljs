9aebc0ba94963d49e8940f03888e075165af5a90
mswift42 <martin.haesler@gmail.com>
2015-11-20 15:57:45 +0000

    Use button group for dropdown menu

--- app/src/cljs/app/components.cljs
+++ app/src/cljs/app/components.cljs
@@ -4,11 +4,16 @@
 
 (defn theme-select
   []
-  [:div.dropdown
-   [:btn.btn-default.dropdown-toggle
-    {:type "button" :id "themedrop" :data-toggle "dropdown"
-     :aria-haspopup "true" :aria-expanded "true"} "Theme Samples"
-    [:span.caret]]
+  [:div.btn-group
+   [:button.btn.btn-default {:type "button"}
+    "Theme Samples"]
+   [:button.btn.btn-default.dropdown-toggle
+    {:type "button" :data-toggle "dropdown"}
+    [:span.caret]
+    [:span.sr-only]
+    ]
    [:ul.dropdown-menu {:aria-labelledby "themedrop"}
-    [:li "white-sand" [:a   {:on-click #(.log js/console "whitesand")}]]]
-   ])
+    [:li [:button.btn.btn-default
+          {:on-click #(db/switch-theme db/white-sand)}
+          "white-sand"] ]
+    [:li [:span  {:on-click #(db/switch-theme db/greymatters)}"greymatters"]]]])
