0cfcf6b3058279f5723810734d90cd38f7ac7f8f
mswift42 <martin.haesler@gmail.com>
2015-11-20 22:25:11 +0000

    add darken method

--- app/src/cljs/app/colors.cljs
+++ app/src/cljs/app/colors.cljs
@@ -1,2 +1,8 @@
 (ns app.colors
   (:require [goog.color :as color]))
+
+(defn darken
+  ([colorstring] (color/rgbArrayToHex
+                  (color/darken (color/hexToRgb colorstring) 0.2)))
+  ([colorstring factor]
+   (color/rgbArrayToHex (color/darken (color/hexToRgb colorstring) factor))))
