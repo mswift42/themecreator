3b5c59559682718a4399200cd9f218cd55d86aa5
mswift42 <martin.haesler@gmail.com>
2015-11-20 16:13:31 +0000

    add soft-charcoal dropdown selection.

--- app/src/cljs/app/components.cljs
+++ app/src/cljs/app/components.cljs
@@ -13,8 +13,14 @@
     [:span.sr-only]
     ]
    [:ul.dropdown-menu {:aria-labelledby "themedrop"}
-    [:li [:a {:href "#" :on-click #(db/switch-theme db/warm-night)} "warm-night"]]
-    [:li [:a {:href "#" :on-click #(db/switch-theme db/white-sand)}
+    [:li [:a
+          {:href "#" :on-click #(db/switch-theme db/warm-night)} "warm-night"]]
+    [:li [:a
+          {:href "#" :on-click #(db/switch-theme db/white-sand)}
           "white-sand"] ]
-    [:li [:a  {:href "#" :on-click #(db/switch-theme db/greymatters)}"greymatters"]]
-    [:li [:a {:href "#" :on-click #(db/switch-theme db/oldlace)} "oldlace"]]]])
+    [:li [:a
+          {:href "#" :on-click #(db/switch-theme db/greymatters)}"greymatters"]]
+    [:li [:a
+          {:href "#" :on-click #(db/switch-theme db/oldlace)} "oldlace"]]
+    [:li [:a
+          {:href "#" :on-click #(db/switch-theme db/soft-charcoal)} "soft-charcoal"]]]])
