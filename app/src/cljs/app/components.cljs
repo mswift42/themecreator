0d1868eaecf367260bd149d748d49fea049105a8
mswift42 <martin.haesler@gmail.com>
2015-11-20 14:36:04 +0000

    add theme-select dropdown

--- app/src/cljs/app/components.cljs
+++ app/src/cljs/app/components.cljs
@@ -1,3 +1,14 @@
 (ns app.components
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [app.db :as db]))
 
+(defn theme-select
+  []
+  [:div.dropdown
+   [:btn.btn-default.dropdown-toggle
+    {:type "button" :id "themedrop" :data-toggle "dropdown"
+     :aria-haspopup "true" :aria-expanded "true"} "Theme Samples"
+    [:span.caret]]
+   [:ul.dropdown-menu {:aria-labelledby "themedrop"}
+    [:li "white-sand" [:a   {:on-click #(.log js/console "whitesand")}]]]
+   ])
