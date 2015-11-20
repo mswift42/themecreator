036e8d7fb92d23a3a803810ad345003f32a4fd1a
mswift42 <martin.haesler@gmail.com>
2015-11-20 22:41:41 +0000

    add method lighten.

--- app/src/cljs/app/colors.cljs
+++ app/src/cljs/app/colors.cljs
@@ -2,7 +2,19 @@
   (:require [goog.color :as color]))
 
 (defn darken
+  "darken darkens a rgb color by a given factor.
+   if no factor is provided, the color will be darkened 
+   with the factor of 0.2."
   ([colorstring] (darken colorstring 0.2))
   ([colorstring factor]
    (color/rgbArrayToHex
     (color/darken (color/hexToRgb colorstring) factor))))
+
+(defn lighten
+  "lighten lightens a rgb color by a given factor.
+   If no factor is provided, the color will be lightened
+   with the factor of 0.2."
+  ([colorstring] (lighten colorstring 0.2))
+  ([colorstring factor]
+   (color/rgbArrayToHex
+    (color/lighten (color/hexToRgb colorstring) factor))))
