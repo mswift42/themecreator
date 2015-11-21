906f1e7e344f8793e82b68f96bf3b6e57c673392
mswift42 <martin.haesler@gmail.com>
2015-11-21 10:02:03 +0000

    use better formula to calculate brightness

--- app/src/cljs/app/colors.cljs
+++ app/src/cljs/app/colors.cljs
@@ -19,8 +19,13 @@
    (color/rgbArrayToHex
     (color/lighten (color/hexToRgb colorstring) factor))))
 
+
 (defn dark-bg?
   "dark-bg? returns true if the given hex color is 'dark'"
   [color]
-  (let [[h s v] (color/hexToHsv color)]
-    (< v 120)))
+  (let [[r g b] (color/hexToRgb color)]
+    (> (- 1 (/ (+ (* 0.299 r)
+                  (* 0.587 g)
+                  (* 0.114 b))
+               255))
+       0.5)))
