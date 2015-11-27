d7354ebf28174d92f0a207cc0322a3a6960b99b2
mswift42 <martin.haesler@gmail.com>
2015-11-27 17:53:01 +0000

    add function declaration

--- app/src/cljs/app/previews.cljs
+++ app/src/cljs/app/previews.cljs
@@ -15,7 +15,15 @@
    [:br]
    [:br]
    [span-component "function " :keyword]
-   [span-component "eratosthenes" :mainfg]] )
+   [span-component "eratosthenes" :functionname]
+   [span-component "(" :mainfg]
+   [span-component "limit" :constant]
+   [span-component ") {" :mainfg]
+   [:br]
+   [span-component "  var " :keyword]
+   [span-component "primes " :variable]
+   [span-component "= [];" :mainfg]
+   [:br]] )
 
 (defn preview-typescript
   []
