2c3a7ef538589cb00cec0311a440ba61d8d61fc0
mswift42 <martin.haesler@gmail.com>
2016-01-17 08:53:23 +0000

    Update target build

--- app/target/js/app.out/app/components.js
+++ app/target/js/app.out/app/components.js
@@ -59,7 +59,7 @@ return iter__5758__auto__(linklist);
 })()], null)], null);
 });
 app.components.theme_select = (function app$components$theme_select(){
-return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.components.select_component,"themedrop","Theme Samples",new cljs.core.PersistentVector(null, 10, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
+return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.components.select_component,"themedrop","Theme Samples",new cljs.core.PersistentVector(null, 11, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
 return app.db.switch_theme(app.db.black);
 }),"black"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
 return app.db.switch_theme(app.db.white);
@@ -78,6 +78,8 @@ return app.db.switch_theme(app.db.oldlace);
 }),"oldlace"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
 return app.db.switch_theme(app.db.soft_charcoal);
 }),"soft-charcoal"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
+return app.db.switch_theme(app.db.bergen);
+}),"bergen"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [(function (){
 return app.db.switch_theme(app.db.madrid);
 }),"madrid"], null)], null)], null);
 });
