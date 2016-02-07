a1a30aa639fde8f3ee2bac4931241640efe60fbe
mswift42 <martin.haesler@gmail.com>
2016-02-07 10:16:25 +0000

    Update target build

--- app/target/js/app.out/app/app.js
+++ app/target/js/app.out/app/app.js
@@ -34,6 +34,61 @@ return goog.net.XhrIo.send(G__11460,G__11461);
 app.app.generate_template = (function app$app$generate_template(template){
 return app.app.compile_template(template,app.colors.derive_colors_from_theme((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db))));
 });
+/**
+ * strip '#' hash symbol of color string.
+ */
+app.app.strip_hash = (function app$app$strip_hash(color){
+return clojure.string.replace(color,"#","");
+});
+app.app.generate_template_intelli = (function app$app$generate_template_intelli(template){
+return app.app.compile_template(template,cljs.core.into.cljs$core$IFn$_invoke$arity$2(cljs.core.PersistentArrayMap.EMPTY,(function (){var iter__5760__auto__ = (function app$app$generate_template_intelli_$_iter__11474(s__11475){
+return (new cljs.core.LazySeq(null,(function (){
+var s__11475__$1 = s__11475;
+while(true){
+var temp__4425__auto__ = cljs.core.seq(s__11475__$1);
+if(temp__4425__auto__){
+var s__11475__$2 = temp__4425__auto__;
+if(cljs.core.chunked_seq_QMARK_(s__11475__$2)){
+var c__5758__auto__ = cljs.core.chunk_first(s__11475__$2);
+var size__5759__auto__ = cljs.core.count(c__5758__auto__);
+var b__11477 = cljs.core.chunk_buffer(size__5759__auto__);
+if((function (){var i__11476 = (0);
+while(true){
+if((i__11476 < size__5759__auto__)){
+var vec__11482 = cljs.core._nth.cljs$core$IFn$_invoke$arity$2(c__5758__auto__,i__11476);
+var k = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__11482,(0),null);
+var v = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__11482,(1),null);
+cljs.core.chunk_append(b__11477,cljs.core.PersistentArrayMap.fromArray([k,app.app.strip_hash(v)], true, false));
+
+var G__11484 = (i__11476 + (1));
+i__11476 = G__11484;
+continue;
+} else {
+return true;
+}
+break;
+}
+})()){
+return cljs.core.chunk_cons(cljs.core.chunk(b__11477),app$app$generate_template_intelli_$_iter__11474(cljs.core.chunk_rest(s__11475__$2)));
+} else {
+return cljs.core.chunk_cons(cljs.core.chunk(b__11477),null);
+}
+} else {
+var vec__11483 = cljs.core.first(s__11475__$2);
+var k = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__11483,(0),null);
+var v = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__11483,(1),null);
+return cljs.core.cons(cljs.core.PersistentArrayMap.fromArray([k,app.app.strip_hash(v)], true, false),app$app$generate_template_intelli_$_iter__11474(cljs.core.rest(s__11475__$2)));
+}
+} else {
+return null;
+}
+break;
+}
+}),null,null));
+});
+return iter__5760__auto__(app.colors.derive_colors_from_theme((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db))));
+})()));
+});
 app.app.window_url = (function app$app$window_url(){
 var or__4988__auto__ = window.URL;
 if(cljs.core.truth_(or__4988__auto__)){
@@ -48,10 +103,10 @@ var blob = (new Blob([data]));
 var templink = document.getElementById(id);
 templink.href = wu.createObjectURL(blob);
 
-var G__11465 = templink;
-G__11465.setAttribute("download",filename);
+var G__11486 = templink;
+G__11486.setAttribute("download",filename);
 
-return G__11465;
+return G__11486;
 });
 app.app.scroll_to_bottom = (function app$app$scroll_to_bottom(){
 var wb = document.body.scrollHeight;
@@ -66,10 +121,15 @@ return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMP
 return app.app.create_blob(app.app.generate_template(template),id,filename);
 })], null),title], null)], null);
 });
+app.app.template_download_intelli = (function app$app$template_download_intelli(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$li,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$a,new cljs.core.PersistentArrayMap(null, 3, [cljs.core.cst$kw$href,"#",cljs.core.cst$kw$id,"intellilink",cljs.core.cst$kw$on_DASH_click,(function (){
+return app.app.create_blob(app.app.generate_template_intelli((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.intellitemplate) : cljs.core.deref.call(null,app.app.intellitemplate))),"intellilink",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".icls")].join(''));
+})], null),"IntelliJ"], null)], null);
+});
 app.app.template_select_component = (function app$app$template_select_component(){
 return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$btn_DASH_group$templatedrop,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$id,"templatedrop"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$button$btn$btn_DASH_default$dropdown_DASH_toggle,new cljs.core.PersistentArrayMap(null, 3, [cljs.core.cst$kw$type,"button",cljs.core.cst$kw$data_DASH_toggle,"dropdown",cljs.core.cst$kw$on_DASH_click,(function (){
 return app.app.scroll_to_bottom();
-})], null),"Theme Download    ",new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$span$caret], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$span$sr_DASH_only], null)], null),new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$ul_SHARP_downloadsel$dropdown_DASH_menu,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$aria_DASH_labelledby,"templatedrop"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"intellilink","IntelliJ",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".icls")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.intellitemplate) : cljs.core.deref.call(null,app.app.intellitemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"tmthemelink","Textmate",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".tmTheme")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.tmthemetemplate) : cljs.core.deref.call(null,app.app.tmthemetemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"atomlink","Atom","colors.less",(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.atomtemplate) : cljs.core.deref.call(null,app.app.atomtemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"emacslink","Emacs",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".el")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.emacstemplate) : cljs.core.deref.call(null,app.app.emacstemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"vimlink","Vim",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".vim")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.vimtemplate) : cljs.core.deref.call(null,app.app.vimtemplate))], null)], null)], null);
+})], null),"Theme Download    ",new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$span$caret], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$span$sr_DASH_only], null)], null),new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$ul_SHARP_downloadsel$dropdown_DASH_menu,new cljs.core.PersistentArrayMap(null, 1, [cljs.core.cst$kw$aria_DASH_labelledby,"templatedrop"], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download_intelli], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"tmthemelink","Textmate",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".tmTheme")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.tmthemetemplate) : cljs.core.deref.call(null,app.app.tmthemetemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"atomlink","Atom","colors.less",(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.atomtemplate) : cljs.core.deref.call(null,app.app.atomtemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"emacslink","Emacs",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".el")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.emacstemplate) : cljs.core.deref.call(null,app.app.emacstemplate))], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.template_download,"vimlink","Vim",[cljs.core.str(cljs.core.cst$kw$themename.cljs$core$IFn$_invoke$arity$1((cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.db.app_db) : cljs.core.deref.call(null,app.db.app_db)))),cljs.core.str(".vim")].join(''),(cljs.core.deref.cljs$core$IFn$_invoke$arity$1 ? cljs.core.deref.cljs$core$IFn$_invoke$arity$1(app.app.vimtemplate) : cljs.core.deref.call(null,app.app.vimtemplate))], null)], null)], null);
 });
 app.app.store_component = (function app$app$store_component(){
 return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.components.button_component,"store theme",(function (){
@@ -103,11 +163,11 @@ app.db.set_db_from_storage();
 return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$div$row,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.color_components], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.preview_component], null)], null);
 });
 app.app.init = (function app$app$init(){
-var G__11470_11474 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.navbar_component], null);
-var G__11471_11475 = document.getElementById("navcontainer");
-(reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__11470_11474,G__11471_11475) : reagent.core.render_component.call(null,G__11470_11474,G__11471_11475));
+var G__11491_11495 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.navbar_component], null);
+var G__11492_11496 = document.getElementById("navcontainer");
+(reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__11491_11495,G__11492_11496) : reagent.core.render_component.call(null,G__11491_11495,G__11492_11496));
 
-var G__11472 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.theme_component], null);
-var G__11473 = document.getElementById("mainapp");
-return (reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__11472,G__11473) : reagent.core.render_component.call(null,G__11472,G__11473));
+var G__11493 = new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [app.app.theme_component], null);
+var G__11494 = document.getElementById("mainapp");
+return (reagent.core.render_component.cljs$core$IFn$_invoke$arity$2 ? reagent.core.render_component.cljs$core$IFn$_invoke$arity$2(G__11493,G__11494) : reagent.core.render_component.call(null,G__11493,G__11494));
 });
