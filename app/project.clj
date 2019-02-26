e7da47d5a34b5785b77bf654b0f307a322536b4b
mswift42 <martin.haesler@gmail.com>
2019-02-26 15:14:43 +0000

    Add project.clj file.

---
+++ app/project.clj
@@ -0,0 +1,56 @@
+(defproject app "0.1.0-SNAPSHOT"
+  :description "themecreator for various IDE's / editors"
+  :url "https://mswift42.github.io/themecreator"
+  :license {:name "GPL v 3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [reagent "0.8.1"]
+                 [cljsjs/mustache "1.1.0-0"]
+                 [cljsjs/jszip "3.1.3-0"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.18"]]
+
+  :clean-targets ^{:protect false}
+  [:target-path
+   [:cljsbuild :builds :app :compiler :output-dir]
+   [:cljsbuild :builds :app :compiler :output-to]]
+
+  :resource-paths ["public"]
+
+  :figwheel {:http-server-root "."
+             :nrepl-port 7002
+             :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+             :css-dirs ["resources/css"]}
+
+  :cljsbuild {:builds {:app
+                       {:source-paths ["src" "env/dev/cljs"]
+                        :compiler
+                        {:main "app.dev"
+                         :output-to "public/js/app.js"
+                         :output-dir "public/js/out"
+                         :asset-path   "js/out"
+                         :source-map true
+                         :optimizations :none
+                         :pretty-print  true}
+                        :figwheel
+                        {:on-jsload "app.core/mount-root"
+                         :open-urls ["http://localhost:3449/index.html"]}}
+                       :release
+                       {:source-paths ["src" "env/prod/cljs"]
+                        :compiler
+                        {:output-to "public/js/app.js"
+                         :output-dir "public/js/release"
+                         :optimizations :advanced
+                         :infer-externs true
+                         :pretty-print false}}}}
+
+  :aliases {"package" ["do" "clean" ["cljsbuild" "once" "release"]]}
+
+  :profiles {:dev {:source-paths ["src" "env/dev/clj"]
+                   :dependencies [[binaryage/devtools "0.9.10"]
+                                  [figwheel-sidecar "0.5.18"]
+                                  [nrepl "0.6.0"]
+                                  [cider/piggieback "0.4.0"]]}})
