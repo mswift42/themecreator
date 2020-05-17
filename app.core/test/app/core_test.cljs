e920efe159ef54c74402a39ee4bf4d7cbb84549b
mswift42 <martin.haesler@gmail.com>
2020-05-17 08:51:18 +0100

    remove multiply test.

--- app.core/test/app/core_test.cljs
+++ app.core/test/app/core_test.cljs
@@ -1,10 +1,4 @@
 (ns app.core-test
     (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [app.core :refer [multiply]]))
+     [cljs.test :refer-macros [deftest is testing]]))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
-
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
