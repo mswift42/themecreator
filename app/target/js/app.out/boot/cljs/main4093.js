7ed7c604f010b6d8433a81182cbe3f5833b7c867
mswift42 <martin.haesler@gmail.com>
2020-05-26 08:44:35 +0100

    delete app/ folder.

--- app/target/js/app.out/boot/cljs/main4093.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.10.520 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main4093');
-goog.require('cljs.core');
-goog.require('cljs.core.constants');
-goog.require('app.app');
-app.app.init();
