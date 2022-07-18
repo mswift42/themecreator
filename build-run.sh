3cbe0f5628270fb3c46380838e7bd36061fbd314
Pedro A. Aranda Gutierrez <paaguti@gmail.com>
2022-07-18 10:59:43 +0200

    Typo

--- build-run.sh
+++ build-run.sh
@@ -3,5 +3,5 @@ UID=$(id -u)
 GID=$(id -g)
 docker build -t themecreator:test .
 docker run --network=host -it \
-       -u $UD:$GID -v $(pwd)/app.core:/usr/src/app \
+       -u $UID:$GID -v $(pwd)/app.core:/usr/src/app \
        --name test themecreator:test
