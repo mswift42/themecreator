7085230755fc270fe9093f7683964d48478c326d
Pedro A. Aranda Gutierrez <paaguti@gmail.com>
2022-07-21 09:10:30 +0200

    UID and GID are defined and read-only in bash

--- build-run.sh
+++ build-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-UID=$(id -u)
-GID=$(id -g)
+# These are defined in bash
+# UID=$(id -u)
+# GID=$(id -g)
 docker build -t themecreator:test .
 docker run --network=host -it \
        -u $UID:$GID -v $(pwd)/app.core:/usr/src/app \
